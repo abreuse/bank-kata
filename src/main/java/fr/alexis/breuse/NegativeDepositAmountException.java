7622ca3ab6a1d7a322ee3e28eb362f63d2487e94
Alexis <alexis.breuse@gmail.com>
2019-01-11 17:10:31 +0100

    add default message to exception

--- src/main/java/fr/alexis/breuse/NegativeDepositAmountException.java
+++ src/main/java/fr/alexis/breuse/NegativeDepositAmountException.java
@@ -1,4 +1,7 @@
 package fr.alexis.breuse;
 
 public class NegativeDepositAmountException extends Throwable {
+    public NegativeDepositAmountException() {
+        super("A negative amount can't be deposit.");
+    }
 }
